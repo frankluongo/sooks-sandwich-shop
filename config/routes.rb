1907746df4e3c7de319bf2e364a3ea7832840412
Frank Luongo <frank@frankluongo.com>
2019-09-11 09:17:35 -0600

    Set up users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Users
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+
+  # Products
   resources :products
+
+  # Content Pages
   get 'content/index'
   get '/about', to: 'content#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'content#index'
 end
