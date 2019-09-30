ec4f20f59491a3aa00d2da02ff2b8a80ef99e0bf
Frank Luongo <frank@frankluongo.com>
2019-09-30 14:07:48 -0600

    Updates to schema and gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_211824) do
+ActiveRecord::Schema.define(version: 2019_09_30_011439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2019_09_23_211824) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.integer "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id", "user_id"], name: "index_orders_on_product_id_and_user_id"
   end
 
   create_table "products", force: :cascade do |t|
